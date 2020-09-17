fceaa5621cb2441329083ea77ce2e48901bab1a1
joshpled <joshuapleduc@gmail.com>
2020-09-16 23:59:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name, gifts) {
+    var array = []
+    for (let i = 0; i < name.length; i++) {
+      array.push(`Thank you, ${name[i]}, for the wonderful ${gifts} gift!`);
+    }
+    return array
+  }
+
+  function countDown(number){
+      for (let count = number; count >= 0; count--) {
+          console.log(count);
+      }
+  }
